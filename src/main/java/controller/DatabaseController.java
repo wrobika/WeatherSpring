72eb4bc49f551ee5bb2dd74d4f6033e80a70150d
gorzz <38137609+gorzz@users.noreply.github.com>
2018-06-17 02:00:03 +0200

    Dodanie wyswietlania

--- src/main/java/controller/DatabaseController.java
+++ src/main/java/controller/DatabaseController.java
@@ -1,14 +1,14 @@
 package controller;
-
-import hibernate.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import model.*;
 import org.springframework.web.servlet.ModelAndView;
-
+import javax.servlet.http.HttpServletRequest;
 import java.sql.Date;
 import java.sql.Time;
 import java.util.List;
@@ -32,7 +32,6 @@ public class DatabaseController
     public ModelAndView readMeasurements(@RequestParam(name="station", required=false, defaultValue="Kraków") String station, Date date, Time hour)
     {
         Session session = hibernate.HibernateUtil.getSessionFactory().openSession();
-
         Query query = session.createQuery("from Measurements where station = :station AND date = :date AND hour = :hour");
         query.setParameter("station", station);
         query.setParameter("date", date);
@@ -78,4 +77,84 @@ public class DatabaseController
         modelAndView.setViewName("/pogoda");
         return modelAndView;
     }
+
+    /**
+     * pobiera liste stacji pomiarowych i przechodzi do formularza wyboru archiwalnego pomiaru
+     * @return
+     */
+    @GetMapping("/wybierzArchiwalnyPomiar")
+    public ModelAndView chooseArchiwalMeasurements()
+    {
+        Session session = hibernate.HibernateUtil.getSessionFactory().openSession();
+        Query query = session.createQuery("select m.station from Measurements m where m.station is not null" );
+        List<String> list = query.list();
+        session.close();
+
+        ModelAndView modelAndView = new ModelAndView("wybierzArchiwalnyPomiar");
+        modelAndView.addObject("measurementsList",list);
+        modelAndView.setViewName("/wybierzArchiwalnyPomiar");
+        return modelAndView;
+    }
+
+    /**
+     * pobiera z formularza wybor archiwalnej probki i dostarcza go metodzie ktora pobierze odpowiednie dane
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/wybierzArchiwalnyPomiar", method = RequestMethod.POST)
+    public ModelAndView chooseArchiwalMeasurements(HttpServletRequest request)
+    {
+        try{
+
+            String station = request.getParameter("station");
+            String date = request.getParameter("date");
+            String time = request.getParameter("time").replaceAll(":[0-9]{2}", ":00:00");
+
+            ModelAndView modelAndView = new ModelAndView("redirect:/pogodaArchiwalna");
+            modelAndView.addObject("station",station);
+            modelAndView.addObject("date", date);
+            modelAndView.addObject("hour",time);
+            return modelAndView;
+
+        } catch(Exception e) {
+
+            ModelAndView modelAndView = new ModelAndView("redirect:/wybierzArchiwalnyPomiar");
+            return modelAndView;
+
+        }
+    }
+
+    /**
+     * pobiera liste stacji pomiarowych i przechodzi do formularza wyboru aktualnego pomiaru
+     * @return
+     */
+    @GetMapping("/wybierzPomiar")
+    public ModelAndView chooseMeasurements()
+    {
+        Session session = hibernate.HibernateUtil.getSessionFactory().openSession();
+        Query query = session.createQuery("select m.station from Measurements m where m.station is not null" );
+        List<String> list = query.list();
+        session.close();
+
+        ModelAndView modelAndView = new ModelAndView("wybierzPomiar");
+        modelAndView.addObject("measurementsList",list);
+        modelAndView.setViewName("/wybierzPomiar");
+        return modelAndView;
+    }
+
+    /**
+     * pobiera z formularza wybor aktualnej probki i dostarcza go metodzie ktora pobierze odpowiednie dane
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/wybierzPomiar", method = RequestMethod.POST)
+    public ModelAndView chooseMeasurements(HttpServletRequest request)
+    {
+        String station = request.getParameter("station");
+
+        ModelAndView modelAndView = new ModelAndView("redirect:/pogoda");
+        modelAndView.addObject("station",station);
+        return modelAndView;
+    }
+
 }
--- src/main/java/controller/GreetingController.java
+++ src/main/java/controller/GreetingController.java
@@ -1,17 +1,24 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    /**
+     * wyswietla strone powitalna
+     * przyklad:
+     * http://localhost:8080/
+     * @return
+     */
+    @GetMapping("/")
+    public ModelAndView index()
+    {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.setViewName("/index");
+        return modelAndView;
     }
 
 }
